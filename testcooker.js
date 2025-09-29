4c1d2301d917ff01378cd91cd5e89dd24f5504ea
MEL <4melwave@gmail.com>
2025-09-28 23:27:13 -0400

    added cookie control

---
+++ testcooker.js
@@ -0,0 +1,141 @@
+//
+
+function test ()
+{
+    //console.log("LOL!!")
+}
+
+function remc()
+{
+document.cookie = "disallow=didntGet"
+//console.log("disallowed")
+window.location.href = 'http://google.com';
+}
+
+
+function givec()
+{
+    //console.log("POW!")
+    //console.log("here you go :3!!!")
+    document.cookie = "allow=heGotDaCookie"
+    document.cookie = "anotherBullshit=itExists"
+    window.location.href = 'index.html';
+}
+
+
+function redir()
+{
+    document.title = "got to start"
+    //document.title = "whoa!"
+    //this is run every time any page loads on the site.
+    //if allow=heGotDaCookie is present, the user may use the site.
+    //if disallow=didntGet is present, the user is always redirected away. this overrides the allow value.
+    
+    //check if the user has the cookie.
+    //console.log("hi");
+
+
+    var x2 = 900;
+    document.title = "got before decode"
+    var y = decodeURIComponent(document.cookie);
+    var x = y.split("; ");
+    //console.log("the cookie is");
+    //console.log(x);
+    
+
+    var accepted = false
+    var rejected = false
+    //document.title = "got thru decode"
+    //iterate over array to find "allow=heGotDaCookie"
+    //http://192.168.50.22:3030/test
+    for(i = 0; i<x.length ; i++)
+    {
+
+        
+        //console.log(x[i])
+        if(x[i]=="allow=heGotDaCookie")
+        {
+            //console.log("user has the accepted cookie.")
+            accepted = true;
+        }
+
+        if(x[i]=="disallow=didntGet")
+        {
+            //console.log("user has the rejected cookie.")
+            rejected = true;
+        }
+    }
+
+    
+    //document.title = rejected
+    //if the user has neither, redirect to landing.
+    if(!accepted && !rejected)
+    {
+        //console.log("go to landing")
+        window.location.href = 'landing.html';
+    }
+
+    //if the rejected cookie is present, redirect away
+    if(rejected)
+    {
+        //console.log("leave")
+        window.location.href = 'https://google.com';
+    }
+    document.title = rejected
+}
+
+//this is for the landing page only.
+function redirL()
+{
+    //this is run every time any page loads on the site.
+    //if allow=heGotDaCookie is present, the user may use the site.
+    //if disallow=didntGet is present, the user is always redirected away. this overrides the allow value.
+    
+    //check if the user has the cookie.
+    var y = decodeURIComponent(document.cookie)
+    var x = y.split("; ")
+    //console.log("the cookie is")
+    //console.log(x)
+    
+
+    var accepted = false
+    var rejected = false
+    //iterate over array to find "allow=heGotDaCookie"
+    //http://192.168.50.22:3030/test
+    for(i = 0; i<x.length ; i++)
+    {
+
+        
+        //console.log(x[i])
+        if(x[i]=="allow=heGotDaCookie")
+        {
+            console.log("user has the accepted cookie.")
+            accepted = true;
+        }
+
+        if(x[i]=="disallow=didntGet")
+        {
+            //console.log("user has the rejected cookie.")
+            rejected = true;
+        }
+    }
+
+    //if the user has neither, redirect to landing.
+    if(!accepted && !rejected)
+    {
+        //console.log("you are landing")
+        //window.location.href = 'landing';
+    }
+
+    //if the rejected cookie is present, redirect away
+    if(rejected)
+    {
+        //console.log("leave")
+        window.location.href = 'https://google.com';
+    }
+ 
+}
+function leave ()
+{
+
+}
